b96bddd2c79d05ff24525561c476e0229bdf8ad7
248369 <65579295+248369@users.noreply.github.com>
2021-08-08 02:18:17 +0200

    Update iqqr.js

--- iqqr.js
+++ iqqr.js
@@ -1,22 +1,36 @@
-function qrcode()
-{
- 
-   var background = document.getElementById("background").value.substring(1);
-   var foreground = document.getElementById("foreground").value.substring(1);
-   var chartsize = document.getElementById("chartsize").value+"x"+document.getElementById("chartsize").value;
-   var link = 'https://image-charts.com/chart?choe=UTF-8';
-   var text_to_qr=document.getElementById("text_to_qr").value;
-   
-   if(document.getElementById("text_to_qr").value=="")text_to_qr="default value";
-   if(document.getElementById("chartsize").value=="")chartsize="200x200";
-   
-   var esc = window.encodeURIComponent ? window.encodeURIComponent : escape;
-   var url = text_to_qr;
-   var cht = link + "&icqrb=" + background + "&icqrf=" + foreground + "&cht=qr" + "&chs=" + chartsize + "&chl=" + esc(url);
-   
-   //var qrresult = "<img  src='" + cht + "' />";
-  // document.write(qrresult);
-  document.getElementById("qr_img_display").src = cht;
-  window.location.href=document.getElementById("qr_img_display").src;
-  
-}
+#####  Encrypted: decrypt with http://www.fourmilab.ch/javascrypt/
+?HX?c51bea476c292cfa4b3cb616535698b76baead572c2c526716a5e2a6a815
+f23e0d344e9debd2efa31e7470d74c22a7ab29a15f8bfd8f60df1a44cdebaf98
+418c7a1a1648381841af1e90e82cdd64963440c1a193e7bbf0a29851662d902c
+7c1e6666b81bf0d42b64310d739465fabcee11672979632e9e5ee7567a5699f1
+ce022d6f525d75f6c2727bec40936aa22a261342701acd10c86984ac5f64bfc7
+bb6981c6119d2f300661a6d862a8d6f97bd292243120bf7fd3dd28677e0065a8
+7ba6f57832b09ad340a2359ffdd984f35dfd53effa77546cad089a43de283ccb
+9f552607dbe94a7c0425eddfbcd2c446f3bba022cdf3f6293bdbfb14d2d8065e
+b13ee8878d121a31fca22579a9015313a4a06c32aaaa1e56d4a6d775938561f3
+4b73311852e194ef1ead4536b22c349471ac889b9efdafb419d8da1fcfb31e93
+1f352a299c1795369bf0e7a850546eaa3b8228a70b3a964382a14395fed95d43
+6b08db806e72f918d7d5d831b93c6d8a140b607591bcad6353d8abcd5582107f
+037d811eed0cf4a42648bd794aabed1de4048f29c90ce4f82adddb82f8439bee
+392c9b263846c22d0bf0082525dd71250e7eb061482823324f8bf18c483829ff
+bc7d787d1c30f4157ba45f7d886b18682b4186e172c06b10d5883703d4efc184
+c186bf0964a1eac5ffa1d01d1e6dea6adbce9cbbc2eedaf1840c6451d9524431
+6418b942f8d518c3b90231441b48c6e836736a95c28e8df3622d07fd7a362fcd
+1976d8da5991f372b497b6fe38f54c03ec627fe862a57ff1d0efaaeee6ab141d
+68a2df46967d1fabf159cb6f13a88affe7e3bb8a18b5d9e61106df1aeab7293c
+1fbd8f78c3f2a4c13baf5091a3acb175c7c8121ba45a05a40bac1f220476473c
+df6e55e87cc0d75bd97fc7f65705f63350f8ba72acb0df5f7ebeebc87da66282
+b3cc0b0dc40c41784453e260c4958806e3caa13a3aeb1ffc9a53d5b42c5fa4e2
+fc9ab3c985c82cce2cc93380794da491b5411a9eb3f963c09e6b50c8393246b1
+7c84fc3ab2c52c48fe33b731375c5b1ef564e085c83e857fbae3f38ed34d53f2
+ffdac8df8b23ebc6e305c191a7cfb22c6ad4b076f3541bc39f6ff6a987b23b35
+d598428de157034350daf33b392c38bd94ef65f1551afaae9b10ec8d158aa438
+169b3264ec38e5d6b7c160d5ba0837f222aa862ed32ad955d729c19ed887f152
+b599f77d75601f7c0a2e2b0704d2f612b0aef3f9ce02a66fc6bd1bd84e4243a3
+5045b6106de1617aabe2c9873108121ff490e2008c6dc75276fabb30d39e87e0
+9c3807ece4537ca89eb61f4143c6e5611ae62cb497dd5faf7dab4e96b552e940
+3eb8a50206083c8ef6072a30b2c7bbb0e64b1996703983819dcefbc7ce79efbb
+a0423ee0848d3ffc7f646ecc7160cee616d6226234be6d21c9b3a1215d4fceac
+0dac994900b5a5701195acf5e9bac6118f7e4218b9de8eda3455f1d292536f6b
+457c?H
+#####  End encrypted message
